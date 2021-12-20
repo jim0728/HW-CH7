969314216bb96ccecc54e471f32569873466c11e
jim0728 <jimchan0728@gmail.com>
2021-12-20 23:17:06 +0800

    first

---
+++ p15/Project1/Project1/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	FILE *fptr1, *fptr2;
+	char ch;
+	fptr1 = fopen("C:/Users/JackyChan/Desktop/110360219_¸â´º¶£/p15/welcome.txt", "r");
+	fptr2 = fopen("C:/Users/JackyChan/Desktop/110360219_¸â´º¶£/p15/output.txt", "w");
+	if ((fptr1 != NULL) && (fptr2 != NULL))
+	{
+		while ((ch = getc(fptr1)) != EOF)
+			putc(ch, fptr2);
+		fclose(fptr1);
+		fclose(fptr2);
+		printf("ÀÉ®×«þ¨©§¹¦¨!!\n");
+	}
+	else
+		printf("ÀÉ®×¶}±Ò¥¢±Ñ!!\n");
+	system("pause");
+	return 0;
+}
